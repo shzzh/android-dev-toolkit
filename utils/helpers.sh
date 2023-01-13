9ba0276279921548d8c02094bd72389e2e5c3efc
shzzh <2302779091@qq.com>
2023-01-13 00:00:00 -0500

    Improve logging system

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -399,3 +399,6 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-05-16T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-01-13T00:00:00
