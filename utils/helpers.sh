ad3bf75aa9fa1371dfbde08354bd7b13f8b85544
shzzh <2302779091@qq.com>
2023-01-22 00:00:00 -0500

    Release stable version

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -639,3 +639,6 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-12-26T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-01-22T00:00:00
