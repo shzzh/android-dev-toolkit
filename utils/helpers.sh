addb9080475e28e2133e23cbe645c9dd48d07725
shzzh <2302779091@qq.com>
2023-01-25 00:00:00 -0500

    Maintenance update

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1377,3 +1377,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-04-07T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-04-07T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2023 at 2023-01-25T00:00:00
