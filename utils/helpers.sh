7cb66828417a5e10e06df3c49cab932206400947
shzzh <2302779091@qq.com>
2023-02-14 00:00:00 -0500

    Add unit tests for core

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1292,3 +1292,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-02-14T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-02-14T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2023 at 2023-02-14T00:00:00
