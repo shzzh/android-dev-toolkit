dede328a715c8b28208a367124377fb2ef0b6d48
shzzh <2302779091@qq.com>
2023-02-14 00:00:00 -0500

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1294,3 +1294,6 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-02-14T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-02-14T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-02-14T00:00:00
