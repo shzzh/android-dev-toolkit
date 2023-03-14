20040a4891e5095ba9a225e0b2198e26532c15dc
shzzh <2302779091@qq.com>
2023-03-14 00:00:00 -0400

    Initial architecture design

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -49,3 +49,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-03-14T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-03-14T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2023 at 2023-03-14T00:00:00
