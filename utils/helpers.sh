094848fd7d3c292302d2d6f38ead4f504dae7f89
shzzh <2302779091@qq.com>
2023-03-16 00:00:00 -0400

    Improve logging system

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1587,3 +1587,6 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-05-07T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-03-16T00:00:00
