72b8cd2567144b177cd80412c4a752f0c3a280f6
shzzh <2302779091@qq.com>
2023-03-23 00:00:00 -0400

    Implement helper utilities

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1050,3 +1050,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-09-04T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-11-18T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-03-23T00:00:00
