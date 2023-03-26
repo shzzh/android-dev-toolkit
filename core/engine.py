ba93155a18f3dd8ae23c9355515220bb3d7d6053
shzzh <2302779091@qq.com>
2023-03-26 00:00:00 -0400

    Refactor engine for performance

--- core/engine.py
+++ core/engine.py
@@ -939,3 +939,5 @@ class Engine:
 # Balanced Update for 2023 at 2023-03-26T00:00:00
     def restart(self): pass
 # Balanced Update for 2023 at 2023-03-26T00:00:00
+    def restart(self): pass
+# Balanced Update for 2023 at 2023-03-26T00:00:00
