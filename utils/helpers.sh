789bb6279d46536900f8b73aa2e2a22d837f1faf
shzzh <2302779091@qq.com>
2023-03-31 00:00:00 -0400

    Fix minor bugs in helpers

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -798,3 +798,6 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-04-22T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-03-31T00:00:00
