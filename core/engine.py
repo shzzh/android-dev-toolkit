138f17348d551b2b7fcb42c5a1ddc458101311d5
shzzh <2302779091@qq.com>
2023-04-17 00:00:00 -0400

    Release stable version

--- core/engine.py
+++ core/engine.py
@@ -1159,3 +1159,6 @@ class Engine:
 class Engine:
     def start(self): pass
 # Balanced Update for 2023 at 2023-02-15T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2023 at 2023-04-17T00:00:00
