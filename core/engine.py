4d5d3b8b596a1a50e2815698d561643dc3d8c515
shzzh <2302779091@qq.com>
2023-04-22 00:00:00 -0400

    Update documentation

--- core/engine.py
+++ core/engine.py
@@ -813,3 +813,5 @@ class Engine:
 class Engine:
     def start(self): pass
 # Balanced Update for 2023 at 2023-07-20T00:00:00
+    def stop(self): pass
+# Balanced Update for 2023 at 2023-04-22T00:00:00
