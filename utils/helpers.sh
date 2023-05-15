7c28164ac5e0285f32d64d199467dd23b15779c9
shzzh <2302779091@qq.com>
2023-05-15 00:00:00 -0400

    Release stable version

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -158,3 +158,5 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-01-11T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2023 at 2023-05-15T00:00:00
