d36553b26613f55c1932a65c2f23f140d896f530
shzzh <2302779091@qq.com>
2023-05-16 00:00:00 -0400

    Maintenance update

--- core/engine.py
+++ core/engine.py
@@ -404,3 +404,5 @@ class Engine:
 # Balanced Update for 2023 at 2023-12-23T00:00:00
     def stop(self): pass
 # Balanced Update for 2023 at 2023-05-16T00:00:00
+    def stop(self): pass
+# Balanced Update for 2023 at 2023-05-16T00:00:00
