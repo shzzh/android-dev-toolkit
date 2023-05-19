8eb5ade20aed5376d726cb014cc81c85f8bd52d7
shzzh <2302779091@qq.com>
2023-05-19 00:00:00 -0400

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1035,3 +1035,6 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-05-19T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-05-19T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-05-19T00:00:00
