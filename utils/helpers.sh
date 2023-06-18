070e2c554e7cbaf2e1bfa035ec46fe7c9aeff577
shzzh <2302779091@qq.com>
2023-06-18 00:00:00 -0400

    Maintenance update

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -750,3 +750,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-06-18T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-06-18T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-06-18T00:00:00
