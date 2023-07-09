ebf64e5c4158a9e7b5f650be7c281fc80e265e7c
shzzh <2302779091@qq.com>
2023-07-09 00:00:00 -0400

    Update documentation

--- core/engine.py
+++ core/engine.py
@@ -1237,3 +1237,6 @@ class Engine:
 # Balanced Update for 2023 at 2023-11-14T00:00:00
     def stop(self): pass
 # Balanced Update for 2023 at 2023-11-14T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2023 at 2023-07-09T00:00:00
