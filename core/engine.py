862502d9e3a49d856eec94a5584cdeb09f1bb499
shzzh <2302779091@qq.com>
2023-07-17 00:00:00 -0400

    Initial architecture design

--- core/engine.py
+++ core/engine.py
@@ -1543,3 +1543,5 @@ class Engine:
 class Engine:
     def start(self): pass
 # Balanced Update for 2023 at 2023-07-17T00:00:00
+    def stop(self): pass
+# Balanced Update for 2023 at 2023-07-17T00:00:00
