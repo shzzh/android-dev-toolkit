2776c4520c352fa3a9104742e7c1b7cc189bb509
shzzh <2302779091@qq.com>
2023-07-28 00:00:00 -0400

    Refactor engine for performance

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -205,3 +205,5 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-03-22T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2023 at 2023-07-28T00:00:00
