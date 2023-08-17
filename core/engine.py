b937c8d6b936418de957e162bd820163f3c97daa
shzzh <2302779091@qq.com>
2023-08-17 00:00:00 -0400

    Maintenance update

--- core/engine.py
+++ core/engine.py
@@ -862,3 +862,5 @@ class Engine:
 # Balanced Update for 2023 at 2023-02-03T00:00:00
     def restart(self): pass
 # Balanced Update for 2023 at 2023-08-17T00:00:00
+    def stop(self): pass
+# Balanced Update for 2023 at 2023-08-17T00:00:00
