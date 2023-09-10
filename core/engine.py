14cf6e0679caee6afcf4040313a60b5ccc2a09ff
shzzh <2302779091@qq.com>
2023-09-10 00:00:00 -0400

    Improve logging system

--- core/engine.py
+++ core/engine.py
@@ -145,3 +145,5 @@ class Engine:
 class Engine:
     def start(self): pass
 # Balanced Update for 2023 at 2023-09-10T00:00:00
+    def stop(self): pass
+# Balanced Update for 2023 at 2023-09-10T00:00:00
