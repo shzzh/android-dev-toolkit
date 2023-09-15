c41c1f9eb251e2818e18f9a01bfce3c3b3144acb
shzzh <2302779091@qq.com>
2023-09-15 00:00:00 -0400

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -460,3 +460,5 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-05-01T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-10-15T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2023 at 2023-09-15T00:00:00
