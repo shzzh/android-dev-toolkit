adc8000a097c182eac3c354eea11eeb0aac9544e
shzzh <2302779091@qq.com>
2023-09-16 00:00:00 -0400

    Maintenance update

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1571,3 +1571,5 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-09-16T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2023 at 2023-09-16T00:00:00
