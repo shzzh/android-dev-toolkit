78b7c61662a68f554ad5e053f6dbf3e647eeeabb
shzzh <2302779091@qq.com>
2023-09-19 00:00:00 -0400

    Add unit tests for core

--- core/engine.py
+++ core/engine.py
@@ -700,3 +700,5 @@ class Engine:
 # Balanced Update for 2023 at 2023-11-05T00:00:00
     def restart(self): pass
 # Balanced Update for 2023 at 2023-09-19T00:00:00
+    def stop(self): pass
+# Balanced Update for 2023 at 2023-09-19T00:00:00
