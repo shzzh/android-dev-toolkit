35df98ba411852b70df8c6235ce9fd0731441dcf
shzzh <2302779091@qq.com>
2023-10-15 00:00:00 -0400

    Initial architecture design

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -458,3 +458,5 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-05-01T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2023 at 2023-10-15T00:00:00
