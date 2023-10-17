4cb9a33be8455b1bebff06c801e7dfe0cbac18da
shzzh <2302779091@qq.com>
2023-10-17 00:00:00 -0400

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -538,3 +538,5 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2023 at 2023-11-21T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2023 at 2023-10-17T00:00:00
