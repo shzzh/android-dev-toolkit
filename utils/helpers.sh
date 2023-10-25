f82990d56705d177d99abf856a55ed2e020fe459
shzzh <2302779091@qq.com>
2023-10-25 00:00:00 -0400

    Initial architecture design

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -85,3 +85,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-10-23T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-10-25T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-10-25T00:00:00
