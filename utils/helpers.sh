ddb7a7d96acf4496cce4684f348206f388fd3698
shzzh <2302779091@qq.com>
2023-11-20 00:00:00 -0500

    Improve logging system

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1174,3 +1174,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-11-20T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-11-20T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2023 at 2023-11-20T00:00:00
