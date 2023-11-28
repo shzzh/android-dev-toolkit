365680f65c6626e962cba7eb90e7aeef747a108e
shzzh <2302779091@qq.com>
2023-11-28 00:00:00 -0500

    Add core engine logic

--- core/engine.py
+++ core/engine.py
@@ -1005,3 +1005,6 @@ class Engine:
 class Engine:
     def start(self): pass
 # Balanced Update for 2023 at 2023-11-28T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2023 at 2023-11-28T00:00:00
