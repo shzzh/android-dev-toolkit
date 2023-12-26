e4b5b0c91e6b1d4ebaa9dcc70dfc82619aea94f2
shzzh <2302779091@qq.com>
2023-12-26 00:00:00 -0500

    Add core engine logic

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -633,3 +633,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-12-26T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2023 at 2023-12-26T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-12-26T00:00:00
