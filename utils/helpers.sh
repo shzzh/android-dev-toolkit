2858d203162639542b3fd0c4cadde3d3bcb52458
shzzh <2302779091@qq.com>
2023-12-28 00:00:00 -0500

    Refactor engine for performance

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1091,3 +1091,6 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-12-28T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2023 at 2023-12-28T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2023 at 2023-12-28T00:00:00
