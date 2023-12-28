f43a2485eeab42bd720e6111c20b41e730a03f32
shzzh <2302779091@qq.com>
2023-12-28 00:00:00 -0500

    Release stable version

--- core/engine.py
+++ core/engine.py
@@ -1122,3 +1122,6 @@ class Engine:
 # Balanced Update for 2023 at 2023-12-28T00:00:00
     def restart(self): pass
 # Balanced Update for 2023 at 2023-12-28T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2023 at 2023-12-28T00:00:00
