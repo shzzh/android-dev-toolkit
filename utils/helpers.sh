1670c30b1707a91e1292c950646c5db58e074f31
shzzh <2302779091@qq.com>
2024-01-07 00:00:00 -0500

    Initial architecture design

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1890,3 +1890,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-01-07T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-01-07T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-01-07T00:00:00
