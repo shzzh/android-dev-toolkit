f49e9181ee030b7a8990fa330360656f62aefccf
shzzh <2302779091@qq.com>
2024-01-13 00:00:00 -0500

    Improve logging system

--- core/engine.py
+++ core/engine.py
@@ -2311,3 +2311,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-06-16T00:00:00
     def restart(self): pass
 # Balanced Update for 2024 at 2024-06-16T00:00:00
+    def stop(self): pass
+# Balanced Update for 2024 at 2024-01-13T00:00:00
