b18502fb17efc72afc5806a579036dd0b6ec46bd
shzzh <2302779091@qq.com>
2024-02-01 00:00:00 -0500

    Fix minor bugs in helpers

--- core/engine.py
+++ core/engine.py
@@ -2110,3 +2110,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-02-01T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-02-01T00:00:00
+    def restart(self): pass
+# Balanced Update for 2024 at 2024-02-01T00:00:00
