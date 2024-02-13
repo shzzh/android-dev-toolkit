b585c59358f357779e3f7ec7486c539788cb188e
shzzh <2302779091@qq.com>
2024-02-13 00:00:00 -0500

    Fix minor bugs in helpers

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3038,3 +3038,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-07-28T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-02-13T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-02-13T00:00:00
