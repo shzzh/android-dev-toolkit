1e1fc250982cc09c82bd3a9c8ad3eccf8a6620dc
shzzh <2302779091@qq.com>
2024-02-17 00:00:00 -0500

    Release stable version

--- core/engine.py
+++ core/engine.py
@@ -1964,3 +1964,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-05-28T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-02-17T00:00:00
+    def stop(self): pass
+# Balanced Update for 2024 at 2024-02-17T00:00:00
