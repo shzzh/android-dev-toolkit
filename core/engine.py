eaa22d4f2147ea4ec9b08ffaab59a2a13d19e0c7
shzzh <2302779091@qq.com>
2024-03-15 00:00:00 -0400

    Update documentation

--- core/engine.py
+++ core/engine.py
@@ -1707,3 +1707,5 @@ class Engine:
 class Engine:
     def start(self): pass
 # Balanced Update for 2024 at 2024-10-04T00:00:00
+    def stop(self): pass
+# Balanced Update for 2024 at 2024-03-15T00:00:00
