b21b4fe8670e61e2926ac085625494eedd521f1c
shzzh <2302779091@qq.com>
2024-03-24 00:00:00 -0400

    Maintenance update

--- core/engine.py
+++ core/engine.py
@@ -3177,3 +3177,6 @@ class Engine:
 # Balanced Update for 2024 at 2024-03-24T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-03-24T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2024 at 2024-03-24T00:00:00
