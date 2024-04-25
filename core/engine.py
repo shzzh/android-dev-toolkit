16acb664534eefd6b6e338d9e5197aac5d125307
shzzh <2302779091@qq.com>
2024-04-25 00:00:00 -0400

    Initial architecture design

--- core/engine.py
+++ core/engine.py
@@ -2284,3 +2284,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-04-25T00:00:00
     def restart(self): pass
 # Balanced Update for 2024 at 2024-04-25T00:00:00
+    def restart(self): pass
+# Balanced Update for 2024 at 2024-04-25T00:00:00
