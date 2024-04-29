1463e42d10607f7bc8f04a2f56c56b5bcfe919db
shzzh <2302779091@qq.com>
2024-04-29 00:00:00 -0400

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3221,3 +3221,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-07-02T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-04-10T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2024 at 2024-04-29T00:00:00
