12329367a59ee2b6c2fa275529a200dcbd3fd9bc
shzzh <2302779091@qq.com>
2024-05-12 00:00:00 -0400

    Fix minor bugs in helpers

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2921,3 +2921,6 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2024 at 2024-05-12T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-05-12T00:00:00
