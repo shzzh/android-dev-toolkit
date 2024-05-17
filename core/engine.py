c48b8a1ad98a911026a030ca56077f0a1fb52936
shzzh <2302779091@qq.com>
2024-05-17 00:00:00 -0400

    Add core engine logic

--- core/engine.py
+++ core/engine.py
@@ -1637,3 +1637,6 @@ class Engine:
 # Balanced Update for 2024 at 2024-05-17T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-05-17T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2024 at 2024-05-17T00:00:00
