dfffa3ab64006b6bb72a98b6d435fbfca184ab1d
shzzh <2302779091@qq.com>
2024-05-19 00:00:00 -0400

    Update documentation

--- core/engine.py
+++ core/engine.py
@@ -3245,3 +3245,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-02-24T00:00:00
     def restart(self): pass
 # Balanced Update for 2024 at 2024-02-24T00:00:00
+    def restart(self): pass
+# Balanced Update for 2024 at 2024-05-19T00:00:00
