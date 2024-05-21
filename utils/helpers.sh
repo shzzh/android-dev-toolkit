da64921c2dadf121ffa13c21fd63f0fa8b358616
shzzh <2302779091@qq.com>
2024-05-21 00:00:00 -0400

    Release stable version

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2602,3 +2602,6 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-01-11T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-01-11T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-05-21T00:00:00
