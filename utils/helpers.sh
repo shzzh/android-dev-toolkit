8f563d14a21073f9edeb73e7b796f54b6698dd11
shzzh <2302779091@qq.com>
2024-05-23 00:00:00 -0400

    Implement helper utilities

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3157,3 +3157,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-05-23T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-05-23T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-05-23T00:00:00
