5cd093d689335d23f2f6f8512222b1b078b6e798
shzzh <2302779091@qq.com>
2024-06-06 00:00:00 -0400

    Release stable version

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1733,3 +1733,6 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2024 at 2024-06-06T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-06-06T00:00:00
