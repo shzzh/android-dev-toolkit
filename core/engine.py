3d7330166121d4c1f342e64f765c689f95626852
shzzh <2302779091@qq.com>
2024-06-07 00:00:00 -0400

    Update documentation

--- core/engine.py
+++ core/engine.py
@@ -2872,3 +2872,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-12-13T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-06-07T00:00:00
+    def restart(self): pass
+# Balanced Update for 2024 at 2024-06-07T00:00:00
