d64ea147cf0f22ffd43a005d27ef025ac7d303ba
shzzh <2302779091@qq.com>
2024-06-07 00:00:00 -0400

    Improve logging system

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2870,3 +2870,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-06-07T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-06-07T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-06-07T00:00:00
