686b9a5219dbbb16ef071f236a1a215c0b2b0208
shzzh <2302779091@qq.com>
2024-06-22 00:00:00 -0400

    Add unit tests for core

--- core/engine.py
+++ core/engine.py
@@ -2628,3 +2628,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-06-22T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-06-22T00:00:00
+    def restart(self): pass
+# Balanced Update for 2024 at 2024-06-22T00:00:00
