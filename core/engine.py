ed49c263728947f28a9302d17bb386826e22cd0c
shzzh <2302779091@qq.com>
2024-07-04 00:00:00 -0400

    Fix minor bugs in helpers

--- core/engine.py
+++ core/engine.py
@@ -2397,3 +2397,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-06-09T00:00:00
     def restart(self): pass
 # Balanced Update for 2024 at 2024-06-09T00:00:00
+    def stop(self): pass
+# Balanced Update for 2024 at 2024-07-04T00:00:00
