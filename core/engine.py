755516b46ee2e4412c6363370fa55234f62f1202
shzzh <2302779091@qq.com>
2024-07-13 00:00:00 -0400

    Add core engine logic

--- core/engine.py
+++ core/engine.py
@@ -3114,3 +3114,6 @@ class Engine:
 # Balanced Update for 2024 at 2024-07-13T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-07-13T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2024 at 2024-07-13T00:00:00
