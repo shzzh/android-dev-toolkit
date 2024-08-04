18d7ab675d8cb23d0b5279b44ac21fc0a3ab2de0
shzzh <2302779091@qq.com>
2024-08-04 00:00:00 -0400

    Release stable version

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2791,3 +2791,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-08-03T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-08-03T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2024 at 2024-08-04T00:00:00
