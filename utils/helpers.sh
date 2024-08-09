db36d5d4dc124fe04290486e51d459fd669e50c5
shzzh <2302779091@qq.com>
2024-08-09 00:00:00 -0400

    Initial architecture design

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1756,3 +1756,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-10-05T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-08-09T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-08-09T00:00:00
