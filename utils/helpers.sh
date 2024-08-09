e1d5bd8cddaa96d48e1076e166c415ee37cf2f38
shzzh <2302779091@qq.com>
2024-08-09 00:00:00 -0400

    Improve logging system

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2516,3 +2516,5 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-12-30T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-08-09T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2024 at 2024-08-09T00:00:00
