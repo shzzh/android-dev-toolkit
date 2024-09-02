c58bb29d7cd046789c9f84c2293682925b27b0a2
shzzh <2302779091@qq.com>
2024-09-02 00:00:00 -0400

    Maintenance update

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2634,3 +2634,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-11-28T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-09-02T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2024 at 2024-09-02T00:00:00
