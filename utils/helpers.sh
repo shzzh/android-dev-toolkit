7549e5e0072a75c9b583f0f13adb431d0a6dc3d8
shzzh <2302779091@qq.com>
2024-09-05 00:00:00 -0400

    Add core engine logic

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2926,3 +2926,6 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-05-12T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-05-12T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-09-05T00:00:00
