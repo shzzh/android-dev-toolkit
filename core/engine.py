cf87c895efce4f4a080b522758270ba10f134c6a
shzzh <2302779091@qq.com>
2024-09-23 00:00:00 -0400

    Refactor engine for performance

--- core/engine.py
+++ core/engine.py
@@ -2349,3 +2349,6 @@ class Engine:
 # Balanced Update for 2024 at 2024-09-20T00:00:00
     def stop(self): pass
 # Balanced Update for 2024 at 2024-09-23T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2024 at 2024-09-23T00:00:00
