29a7bc7892b72ea182468e38d8ade622178e5a19
shzzh <2302779091@qq.com>
2024-09-27 00:00:00 -0400

    Implement helper utilities

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2338,3 +2338,5 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2024 at 2024-09-27T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2024 at 2024-09-27T00:00:00
