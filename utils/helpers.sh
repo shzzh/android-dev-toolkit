8a739a331a3a1a5175424b02670a4e787bdbaf98
shzzh <2302779091@qq.com>
2024-10-24 00:00:00 -0400

    Initial architecture design

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2132,3 +2132,5 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2024 at 2024-10-24T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2024 at 2024-10-24T00:00:00
