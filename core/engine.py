2bdf6f74bccb43331e81cdfa47e9e1f27e65a21d
shzzh <2302779091@qq.com>
2024-11-24 00:00:00 -0500

    Release stable version

--- core/engine.py
+++ core/engine.py
@@ -2836,3 +2836,5 @@ class Engine:
 # Balanced Update for 2024 at 2024-06-16T00:00:00
     def restart(self): pass
 # Balanced Update for 2024 at 2024-11-24T00:00:00
+    def restart(self): pass
+# Balanced Update for 2024 at 2024-11-24T00:00:00
