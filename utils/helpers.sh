922910ab7a222dbff278f38c89907181dbe3f198
shzzh <2302779091@qq.com>
2024-12-13 00:00:00 -0500

    Add core engine logic

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -2850,3 +2850,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-09-21T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-09-21T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2024 at 2024-12-13T00:00:00
