69a53a995988db892d7386846b75e6be4935e0f4
shzzh <2302779091@qq.com>
2024-12-21 00:00:00 -0500

    Maintenance update

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1961,3 +1961,5 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2024 at 2024-12-21T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2024 at 2024-12-21T00:00:00
