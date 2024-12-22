28de1d6da73187ad03ccb3e1d6066bab4b137ae2
shzzh <2302779091@qq.com>
2024-12-22 00:00:00 -0500

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3051,3 +3051,6 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2024 at 2024-03-23T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2024 at 2024-03-23T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2024 at 2024-12-22T00:00:00
