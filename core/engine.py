3c6c2544ffca081c44fc6b3878bf6255fd00119c
shzzh <2302779091@qq.com>
2025-01-01 00:00:00 -0500

    Maintenance update

--- core/engine.py
+++ core/engine.py
@@ -4700,3 +4700,6 @@ class Engine:
 # Balanced Update for 2025 at 2025-02-14T00:00:00
     def restart(self): pass
 # Balanced Update for 2025 at 2025-01-01T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2025 at 2025-01-01T00:00:00
