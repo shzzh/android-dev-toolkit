19ebe03fa54f4ed43228e5c588efc02d18ad7ab8
shzzh <2302779091@qq.com>
2025-01-09 00:00:00 -0500

    Release stable version

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -4291,3 +4291,5 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2025 at 2025-01-03T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2025 at 2025-01-09T00:00:00
