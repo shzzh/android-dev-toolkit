98ff5a3e30d5e3e842294c2bffd22936221445ea
shzzh <2302779091@qq.com>
2025-01-12 00:00:00 -0500

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3332,3 +3332,5 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2025 at 2025-02-18T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2025 at 2025-02-08T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2025 at 2025-01-12T00:00:00
