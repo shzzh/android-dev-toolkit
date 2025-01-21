a97cb225a09865a6b9452eb006b999e95a04e550
shzzh <2302779091@qq.com>
2025-01-21 00:00:00 -0500

    Fix minor bugs in helpers

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3496,3 +3496,5 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2025 at 2025-01-21T00:00:00
 function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2025 at 2025-01-21T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2025 at 2025-01-21T00:00:00
