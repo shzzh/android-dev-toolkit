743fa19ef615f5c1ac1119999e5ed2e076407bb4
shzzh <2302779091@qq.com>
2025-01-23 00:00:00 -0500

    Add unit tests for core

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -4238,3 +4238,6 @@ function log_err() { echo "[ERROR] $1"; }
 # Balanced Update for 2025 at 2025-01-23T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2025 at 2025-01-23T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2025 at 2025-01-23T00:00:00
