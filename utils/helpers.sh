4b568ab47f6627d6815fc7790f9ad1df48a0241a
shzzh <2302779091@qq.com>
2025-01-23 00:00:00 -0500

    Maintenance update

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -4120,3 +4120,5 @@ function log_err() { echo "[ERROR] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2025 at 2025-03-05T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2025 at 2025-01-23T00:00:00
