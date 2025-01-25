d283d41ee53ce1efb66b1b8b97e68477ab71260f
shzzh <2302779091@qq.com>
2025-01-25 00:00:00 -0500

    Release stable version

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -4340,3 +4340,5 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2025 at 2025-01-25T00:00:00
+function log_info() { echo "[INFO] $1"; }
+# Balanced Update for 2025 at 2025-01-25T00:00:00
