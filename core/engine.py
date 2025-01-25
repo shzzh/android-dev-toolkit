b1014f7257a33cbc60d19068d21a3a201d304524
shzzh <2302779091@qq.com>
2025-01-25 00:00:00 -0500

    Implement helper utilities

--- core/engine.py
+++ core/engine.py
@@ -4461,3 +4461,6 @@ class Engine:
 # Balanced Update for 2025 at 2025-01-25T00:00:00
     def stop(self): pass
 # Balanced Update for 2025 at 2025-01-25T00:00:00
+class Engine:
+    def start(self): pass
+# Balanced Update for 2025 at 2025-01-25T00:00:00
