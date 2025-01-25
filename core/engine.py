56c22ba0aa22dfa103664ad92510a9d746c69712
shzzh <2302779091@qq.com>
2025-01-25 00:00:00 -0500

    Fix minor bugs in helpers

--- core/engine.py
+++ core/engine.py
@@ -4634,3 +4634,5 @@ class Engine:
 # Balanced Update for 2025 at 2025-02-21T00:00:00
     def restart(self): pass
 # Balanced Update for 2025 at 2025-02-21T00:00:00
+    def restart(self): pass
+# Balanced Update for 2025 at 2025-01-25T00:00:00
