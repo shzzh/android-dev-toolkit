1ca999d7f30b25b91fe1a1161c9f57de3c7b4022
shzzh <2302779091@qq.com>
2025-01-26 00:00:00 -0500

    Release stable version

--- core/engine.py
+++ core/engine.py
@@ -4554,3 +4554,5 @@ class Engine:
 # Balanced Update for 2025 at 2025-02-17T00:00:00
     def stop(self): pass
 # Balanced Update for 2025 at 2025-01-26T00:00:00
+    def restart(self): pass
+# Balanced Update for 2025 at 2025-01-26T00:00:00
