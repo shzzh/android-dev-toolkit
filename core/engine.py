b78a1c253cceac49d144fa7669bdea1d55d77d91
shzzh <2302779091@qq.com>
2025-02-01 00:00:00 -0500

    Maintenance update

--- core/engine.py
+++ core/engine.py
@@ -4065,3 +4065,5 @@ class Engine:
 class Engine:
     def start(self): pass
 # Balanced Update for 2025 at 2025-02-01T00:00:00
+    def restart(self): pass
+# Balanced Update for 2025 at 2025-02-01T00:00:00
