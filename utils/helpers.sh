b067cb4363e8b90509bfa08eed01bb08ee711bfb
shzzh <2302779091@qq.com>
2025-02-01 00:00:00 -0500

    Update documentation

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3914,3 +3914,5 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2025 at 2025-02-27T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2025 at 2025-02-01T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2025 at 2025-02-01T00:00:00
