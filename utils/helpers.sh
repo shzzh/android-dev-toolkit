b5dd7970aa2206dea944b34bba36cafb3f8e0cb5
shzzh <2302779091@qq.com>
2025-02-11 00:00:00 -0500

    Fix minor bugs in helpers

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -3304,3 +3304,6 @@ function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2025 at 2025-01-26T00:00:00
 function log_info() { echo "[INFO] $1"; }
 # Balanced Update for 2025 at 2025-01-26T00:00:00
+#!/bin/bash
+# Helper functions
+# Balanced Update for 2025 at 2025-02-11T00:00:00
