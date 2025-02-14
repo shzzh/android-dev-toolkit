dfdce8399201d6aadcd2a3432893d7bf16aacc78
shzzh <2302779091@qq.com>
2025-02-14 00:00:00 -0500

    Improve logging system

--- utils/helpers.sh
+++ utils/helpers.sh
@@ -4471,3 +4471,5 @@ function log_info() { echo "[INFO] $1"; }
 #!/bin/bash
 # Helper functions
 # Balanced Update for 2025 at 2025-02-14T00:00:00
+function log_err() { echo "[ERROR] $1"; }
+# Balanced Update for 2025 at 2025-02-14T00:00:00
