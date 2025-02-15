66c0d7d50d1b40c60b524248d1c3eb61647c92cf
shzzh <2302779091@qq.com>
2025-02-15 00:00:00 -0500

    Add core engine logic

--- core/engine.py
+++ core/engine.py
@@ -3758,3 +3758,5 @@ class Engine:
 # Balanced Update for 2025 at 2025-02-27T00:00:00
     def restart(self): pass
 # Balanced Update for 2025 at 2025-02-15T00:00:00
+    def stop(self): pass
+# Balanced Update for 2025 at 2025-02-15T00:00:00
